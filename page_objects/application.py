3fb2208b56cd37e5ee6fa4f96a2961d95721dc11
Larysa Uroshlieva <lora.strizhakova@gmail.com>
2022-12-20 15:58:32 +0200

    Add page object

--- page_objects/application.py
+++ page_objects/application.py
@@ -1,40 +1,36 @@
 from playwright.sync_api import Playwright
 
-class Complex:
-    def __init__(self, realpart, imagpart):
-        self.r = realpart
-        self.i = imagpart
+from .test_cases import TestCases
 
 
 class App:
-    def __init__(self, playwright: Playwright):
-        # self.playwright = playwright
-        self.browser = playwright.chromium.launch(headless=False, devtools=True)
+    def __init__(self, playwright: Playwright, base_url: str, headless=False):
+        self.browser = playwright.chromium.launch(headless=False)
         self.context = self.browser.new_context()
         self.page = self.context.new_page()
+        self.base_url = base_url
+        self.test_cases = TestCases(self.page)
 
-    def login(self):
+    def goto(self, endpoint: str, use_base_url=True):
+        if use_base_url:
+            self.page.goto(self.base_url + endpoint)
+        else:
+            self.page.goto(endpoint)
+
+    def navigate_to_menu(self, menu: str):
+        self.page.locator(f"text={menu}").click()
+
+    def login(self, login: str, password: str):
         self.page.goto("http://127.0.0.1:8000/login/?next=/")
-        self.page.locator("input[name=\"username\"]").fill("alice")
-        self.page.locator("input[name=\"password\"]").fill("Qamania123")
+        self.page.locator("input[name=\"username\"]").fill(login)
+        self.page.locator("input[name=\"password\"]").fill(password)
         self.page.locator("text=Login").click()
 
-    def create_test(self):
+    def create_test(self, test_name: str, test_description: str):
         self.page.locator("text=Create new test").click()
-        self.page.locator("input[name=\"name\"]").fill("test111")
-        self.page.locator("textarea[name=\"description\"]").fill("description111")
+        self.page.locator("input[name=\"name\"]").fill(test_name)
+        self.page.locator("textarea[name=\"description\"]").fill(test_description)
         self.page.locator("input:has-text(\"Create\")").click()
-
-    def open_tests(self):
-        self.page.locator("text=Test Cases").click()
-        self.page.wait_for_url("http://127.0.0.1:8000/tests/")
-
-    def check_tests_created(self):
-        assert self.page.query_selector('//td[text()="test111"]') is not None
-
-    def delete_test(self):
-        self.page.locator("text=test111 description111 alice Norun None PASS FAIL Details Delete >> button").nth(
-            3).click()
 
     def close(self):
         self.page.close()
--- readme.md
+++ readme.md
@@ -1,8 +1,10 @@
 # Project to learn and demonstrate web testing using pytest + playwright
 
 ## Install guide
+
 ...
 
-## Useful links 
+## Useful links
+
 https://playwright.dev/python/docs/intro
 https://github.com/Microsoft/playwright-python
