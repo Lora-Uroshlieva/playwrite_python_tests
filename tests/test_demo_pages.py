e3c88dc50eebfe073d8b340cb093d8da0fb33974
Larysa Uroshlieva <lora.strizhakova@gmail.com>
2023-01-06 13:40:43 +0200

    Add test for DemoPages

---
+++ tests/test_demo_pages.py
@@ -0,0 +1,15 @@
+def test_wait_more_30s(desktop_app_auth):
+    desktop_app_auth.navigate_to_menu('Demo pages')
+    desktop_app_auth.demo_pages.open_page_after_wait(8)
+    assert desktop_app_auth.demo_pages.check_wait_page()
+
+
+def test_ajax(desktop_app_auth):
+    desktop_app_auth.navigate_to_menu('Demo pages')
+    desktop_app_auth.demo_pages.open_page_and_wait_ajax(3)
+    assert 3 == desktop_app_auth.demo_pages.get_ajax_responses_count()
+
+
+def test_testcase_does_not_exist(desktop_app_auth):
+    desktop_app_auth.navigate_to_menu('Demo pages')
+    assert not desktop_app_auth.test_cases.check_test_exists('this test definitely does not exist')
