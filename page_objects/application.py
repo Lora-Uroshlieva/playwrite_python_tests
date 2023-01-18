2e4738e4ea39d387f8a951557f378ca0ec2c1056
Larysa Uroshlieva <lora.strizhakova@gmail.com>
2023-01-18 12:41:23 +0200

    Add allure reporting

--- page_objects/application.py
+++ page_objects/application.py
@@ -1,5 +1,6 @@
 import logging
 
+import allure
 from playwright.sync_api import Browser, Route, Request, ConsoleMessage, Dialog
 
 from .demo_pages import DemoPages
@@ -26,51 +27,63 @@ class App:
         self.page.on('console', console_handler)
         self.page.on('dialog', dialog_handler)
 
+    @allure.step
     def goto(self, endpoint: str, use_base_url=True):
         if use_base_url:
             self.page.goto(self.base_url + endpoint)
         else:
             self.page.goto(endpoint)
 
+    @allure.step
     def navigate_to_menu(self, menu: str):
-        self.page.locator(f"text={menu}").click()
+        self.page.locator('.menuBox').locator(f"text={menu}").click()
         self.page.wait_for_load_state()
 
+    @allure.step
     def login(self, login: str, password: str):
         self.page.locator("input[name=\"username\"]").fill(login)
         self.page.locator("input[name=\"password\"]").fill(password)
         self.page.locator("text=Login").click()
 
+    @allure.step
     def create_test(self, test_name: str, test_description: str):
         self.page.locator("text=Create new test").click()
         self.page.locator("input[name=\"name\"]").fill(test_name)
         self.page.locator("textarea[name=\"description\"]").fill(test_description)
         self.page.locator("input:has-text(\"Create\")").click()
 
+    @allure.step
     def click_menu_button(self):
         self.page.locator(".menuBtn").click()
 
+    @allure.step
     def is_menu_button_visible(self):
         return self.page.locator(".menuBtn").is_visible()
 
+    @allure.step
     def get_location(self):
         return self.page.locator('.position').text_content()
 
+    @allure.step
     def intercept_requests(self, url: str, payload: str):
         def handler(route: Route, request: Request):
             route.fulfill(status=200, body=payload)
 
         self.page.route(url, handler)
 
+    @allure.step
     def stop_intercept(self, url: str):
         self.page.unroute(url)
 
+    @allure.step
     def refresh_dashboard(self):
         self.page.locator('.refresh').click()
 
+    @allure.step
     def get_total_tests_stats(self):
         return self.page.text_content('.total >> span')
 
+    @allure.step
     def close(self):
         self.page.close()
         self.context.close()
